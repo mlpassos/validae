581b3cc4e99d0e12515b7644662bce4e6fe63052
Márcio Passos <marciopassosbel@gmail.com>
2016-06-11 23:40:04 -0300

    Update
    Ajax chain testing updates

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -40,88 +40,133 @@
 				});
 				return;
 			}
-        	$('body').delegate('.tabela-resultados-mensagem-botao','click',function(){
-        		// todo
-        	});
-        	// horrible way of getting all the pages results (todo way better $deffering)
-        	$('body').delegate('.validar-todas','click',function(){
-        		var tempo = 500;
-        		$('.tabela-resultados-corpo > tr > td > .verificar-url').each(function(index,item){
-        			var linha = $(this);
-					linha.delay(tempo).fadeToggle(tempo, function(){
-						$(this).trigger('click');
-					});
-					tempo += 500;
-        		});
-        	});
-        	// validate single url
-        	$('body').delegate('.verificar-url', 'click', function(){
-        		var url = encodeURIComponent($(this).attr('data-url'));
-        		var botao = $(this);
-        		botao.html('<img style="width:16px;height:16px;" src="http://localhost/validagit/validae//assets/images/ajaxloading.gif">');
-        		$.ajax({
-		            type: "get",
-		            url: 'https://validator.w3.org/nu/?doc=' + url + '&out=json&parser=html5',
-		           	success: function(data) {
-		           		// console.log(typeof data.messages);
-		           		botao.html('');
-		           		if (jQuery.isEmptyObject(data.messages)) {
-		           			botao.text('w3c válido');
-		           			botao.parent().parent().addClass('bg-success');
-		           		} else {
-		           			// search for errors and infos/warnings and store into array
-		           			var  arrErro = [];
-		           			var  arrInfo = [];
-		           			var  arrWarning = [];
-		           			botao.parent().parent().addClass('bg-danger');
-		           			botao.text('Erro').removeClass('btn-succcess').addClass('btn-danger');
-			           		data.messages.forEach(function(item,i) {
-			           			// console.log(item.type);
-			           			if (item.type=="error") {
-			           				arrErro.push(item);
-			           			} else if (item.type=="info") {
-			           				arrInfo.push(item);
-			           			} 
-			           		});
-			           		// set up modals with different arrays (todo way better)
+
+    	$('body').delegate('.tabela-resultados-mensagem-botao','click',function(){
+    		// todo
+    	});
+    	// horrible way of getting all the pages results (todo way better $deffering)
+   	
+    	function getFile(el, url, cb) {
+    		url = encodeURIComponent(url);
+    		el.html('<img style="width:16px;height:16px;" src="http://localhost/validagit/validae//assets/images/ajaxloading.gif">');
+    		$.ajax({
+            type: "get",
+            url: 'https://validator.w3.org/nu/?doc=' + url + '&out=json&parser=html5',
+           	dataType: 'json'
+        }).done(cb);
+    	}
+
+    	$('body').delegate('.validar-todas','click',function(){
+    		var arr = $('.tabela-resultados-corpo > tr > td > .verificar-url');
+  			arr.each(function(index,item){
+    			var el = $(this);
+    			var url = $(this).attr('data-url');
+    			getFile(el, url, function(data) {
+    				// console.log(Date.now());
+    				el.html('');
+         		if (jQuery.isEmptyObject(data.messages)) {
+         			el.text('w3c válido');
+         			el.parent().parent().addClass('bg-success');
+         		} else {
+         			// search for errors and infos/warnings and store into array
+         			var  arrErro = [];
+         			var  arrInfo = [];
+         			var  arrWarning = [];
+         			el.parent().parent().addClass('bg-danger');
+         			el.text('Erro').removeClass('btn-succcess').addClass('btn-danger');
+           		data.messages.forEach(function(item,i) {
+           			// console.log(item.type);
+           			if (item.type=="error") {
+           				arrErro.push(item);
+           			} else if (item.type=="info") {
+           				arrInfo.push(item);
+           			} 
+           		});
+           		// set up modals with different arrays (todo way better)
 							if (arrInfo.length>0) {
-								botao.parent().parent().next('.tabela-resultados-mensagem')
-	           						.find('.panel-body').append('<button data-toggle="modal" data-target="#modalInfo" data-dados="' + arrInfo + '" type="button" class="btn btn-xs btn-info tabela-resultados-mensagem-botao">Info <span class="badge">' + arrInfo.length + '</span></button>');
-	           					modal('info', arrInfo);
+								el.parent().parent().next('.tabela-resultados-mensagem')
+		         						.find('.panel-body').append('<button data-toggle="modal" data-target="#modalInfo" data-dados="' + arrInfo + '" type="button" class="btn btn-xs btn-info tabela-resultados-mensagem-botao">Info <span class="badge">' + arrInfo.length + '</span></button>');
+		         					modal('ielnfo', arrInfo);
 							} 
 							if (arrErro.length>0) {
-								botao.parent().parent().next('.tabela-resultados-mensagem')
-	           						.find('.panel-body').append('<button data-toggle="modal" data-target="#modalErro" data-dados="' + arrErro + '" type="button" class="btn btn-xs btn-danger tabela-resultados-mensagem-botao">Errors <span class="badge">' + arrErro.length + '</span></button>');
+								el.parent().parent().next('.tabela-resultados-mensagem')
+		         						.find('.panel-body').append('<button data-toggle="modal" data-target="#modalErro" data-dados="' + arrErro + '" type="button" class="btn btn-xs btn-danger tabela-resultados-mensagem-botao">Errors <span class="badge">' + arrErro.length + '</span></button>');
 								modal('erro', arrErro);
 							}
-							botao.parent().parent().next('.tabela-resultados-mensagem').slideToggle('slow');
-		           		}
-		           	},
-		           	error: function(error) {
-		           		console.log(error);
-		           	},
-		           	dataType: 'json'
-		        }).done(function(data, status){
-		        	// done
-		        	// console.log('terminou: ' + status);
-		        });
-        	});
-			// search pages in a website
-        	$('#formSiteUrl').submit(function(e) {
-        		e.preventDefault();
-        		var url = $('#strPesquisar').val();
-        		var total = "";
-        		if ($('.tabela-resultados').css('display')=="table") {
-    				$('.tabela-resultados').slideToggle('slow','linear', function(){
-    					$('.tabela-resultados-corpo').html('');	
-    					$('.resultados-total').slideToggle('slow', 'linear', function(){
-    						$('.ajax-loading').show();	
-    					});
-    				});
+							el.parent().parent().next('.tabela-resultados-mensagem').slideToggle('slow');
+         		}
+    			});
+  			});
+    	});
+
+    	// validate single url
+    	$('body').delegate('.verificar-url', 'click', function(){
+    		var url = encodeURIComponent($(this).attr('data-url'));
+    		var botao = $(this);
+    		botao.html('<img style="width:16px;height:16px;" src="http://localhost/validagit/validae//assets/images/ajaxloading.gif">');
+    		$.ajax({
+            type: "get",
+            url: 'https://validator.w3.org/nu/?doc=' + url + '&out=json&parser=html5',
+           	success: function(data) {
+           		// console.log(typeof data.messages);
+           		botao.html('');
+           		if (jQuery.isEmptyObject(data.messages)) {
+           			botao.text('w3c válido');
+           			botao.parent().parent().addClass('bg-success');
+           		} else {
+           			// search for errors and infos/warnings and store into array
+           			var  arrErro = [];
+           			var  arrInfo = [];
+           			var  arrWarning = [];
+           			botao.parent().parent().addClass('bg-danger');
+           			botao.text('Erro').removeClass('btn-succcess').addClass('btn-danger');
+	           		data.messages.forEach(function(item,i) {
+	           			// console.log(item.type);
+	           			if (item.type=="error") {
+	           				arrErro.push(item);
+	           			} else if (item.type=="info") {
+	           				arrInfo.push(item);
+	           			} 
+	           		});
+	           		// set up modals with different arrays (todo way better)
+								if (arrInfo.length>0) {
+									botao.parent().parent().next('.tabela-resultados-mensagem')
+			         						.find('.panel-body').append('<button data-toggle="modal" data-target="#modalInfo" data-dados="' + arrInfo + '" type="button" class="btn btn-xs btn-info tabela-resultados-mensagem-botao">Info <span class="badge">' + arrInfo.length + '</span></button>');
+			         					modal('info', arrInfo);
+								} 
+								if (arrErro.length>0) {
+									botao.parent().parent().next('.tabela-resultados-mensagem')
+			         						.find('.panel-body').append('<button data-toggle="modal" data-target="#modalErro" data-dados="' + arrErro + '" type="button" class="btn btn-xs btn-danger tabela-resultados-mensagem-botao">Errors <span class="badge">' + arrErro.length + '</span></button>');
+									modal('erro', arrErro);
+								}
+								botao.parent().parent().next('.tabela-resultados-mensagem').slideToggle('slow');
+           		}
+           	},
+           	error: function(error) {
+           		console.log(error);
+           	},
+           	dataType: 'json'
+        }).done(function(data, status){
+        	// done
+        	// console.log('terminou: ' + status);
+        });
+    	});
+	// search pages in a website
+    	$('#formSiteUrl').submit(function(e) {
+    		e.preventDefault();
+    		var url = $('#strPesquisar').val();
+    		var total = "";
+    		if ($('.tabela-resultados').css('display')=="table") {
+					$('.tabela-resultados').slideToggle('slow','linear', function(){
+						$('.tabela-resultados-corpo').html('');	
+						$('.resultados-total').slideToggle('slow', 'linear', function(){
+							$('.ajax-loading').show();	
+						});
+					});
 				} else {
 					$('.ajax-loading').show();
 				}
-        		$.ajax({
+		    		$.ajax({
 		            type: "post",
 		            url: window.location.href + "/validar",
 		            data: {
@@ -162,8 +207,8 @@
 		            dataType: 'json'
 		        }).done(function(data,status){
 		        	$('.ajax-loading').hide();
-	        		//console.log(status);
-	        		if (status=="success") {
+		      		//console.log(status);
+		      		if (status=="success") {
 			        	$('.resultados-total').slideToggle('slow', function(){
 			        		$('.tabela-resultados').slideToggle('slow');	
 			        	});
@@ -171,8 +216,8 @@
 		       			$('.principal').text('Deu bug... tente novamente? =]');
 		        	}	
 		        });
-        	});
-		}
+		    	});
+				}
 	}
     $(document).ready(function () {
         fn.Iniciar();
